532e4b64b8a76482b9cbadac7918ac7feead73e9
Alecio Furanze (Ale) <afuranze@kezero.com>
2025-12-10 23:27:17 +0200

    fix. Migrations

---
+++ server/Migrations/20251210212518_FixBlobModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KePass.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixBlobModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Blob");
+
+            migrationBuilder.CreateTable(
+                name: "Blobs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Size = table.Column<long>(type: "INTEGER", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blobs", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Blobs_Name",
+                table: "Blobs",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Blobs");
+
+            migrationBuilder.CreateTable(
+                name: "Blob",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Name = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Size = table.Column<long>(type: "INTEGER", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blob", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Blob_Name",
+                table: "Blob",
+                column: "Name",
+                unique: true);
+        }
+    }
+}
